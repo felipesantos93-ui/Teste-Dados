42279da18bf6328d3bb9ab668890ec8ff94d165d
felipesantos93-ui <alemaosantos93@gmail.com>
2026-03-04 20:40:00 +0000

    Primeiro app no StreamLit

---
+++ app.py
@@ -0,0 +1,14 @@
+#import streamlit as st
+
+#st.write("""Meu Primeiro teste com Streamlit""")
+
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(
+    rng(0).standard_normal((1000, 2)) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+
+st.map(df)
